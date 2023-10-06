4d0d908dcbda999275eff0485f6b2f9b9f763a49
Maxtop <upworkgoes@gmail.com>
2023-10-06 03:31:58 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['firebasestorage.googleapis.com'],
-      }
-}
+  images: {
+    domains: ["firebasestorage.googleapis.com"],
+  },
+  ignoreDuringBuilds: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
