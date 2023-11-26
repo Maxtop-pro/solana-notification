25a8543695e181e7e49781f3196d0a20ae743312
Maxtop <upworkgoes@gmail.com>
2023-11-26 07:42:43 +0100

    pirate

---
+++ src/app/pirate/privacy/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="p-6 rounded-md shadow-md">
+        <h1 className="text-2xl font-bold mb-4">Pirate's Guess Privacy Policy</h1>
+        <p className="mb-4">
+          Pirate's Guess is a game that does not collect any user data. We respect your privacy
+          and do not store any personal information.
+        </p>
+        <p className="mb-4">
+          As a user, you can enjoy playing the game without worrying about your data being
+          collected or shared.
+        </p>
+        <p className="mb-4">
+          If you have any questions or concerns regarding privacy, feel free to contact us at
+          hojoisaac@gmail.com.
+        </p>
+      </div>
+    </div>
+  );
+}
