a1b42d47bc274dc57ec7ff5fe592275563cb6295
Maxtop <upworkgoes@gmail.com>
2024-01-25 17:13:29 +0100

    Create kini privacy policy

---
+++ src/app/kini/privacy/page.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="flex flex-col mt-20 ml-20">
+    <h1 className="text-2xl font-bold mb-4">Kini Privacy Policy</h1>
+        <p className="mb-4">
+          Kini is a dictionary app committed to your privacy. We do not collect any personal data or user identifiers.
+        </p>
+        <h2 className="text-xl font-semibold mb-3">AdMob</h2>
+        <p className="mb-4">
+          We use Google&apos;s AdMob advertising service to display ads within the app. AdMob may collect and use information to provide personalized advertising. This includes non-personally identifiable information specific to the ad serving process.
+        </p>
+        <p className="mb-4">
+            Please refer to Google&apos;s Privacy Policy for more details on how they manage the data they collect.
+        </p>
+        <h2 className="text-xl font-semibold mb-3">Search Queries</h2>
+        <p className="mb-4">
+          Any search queries you make within Kini are processed by OpenAI to provide dictionary results. These queries are not stored by Kini and are only used to generate immediate search results.
+        </p>
+        <h2 className="text-xl font-semibold mb-3">Data Protection</h2>
+        <p className="mb-4">
+          We are dedicated to protecting your data and ensuring that our third-party partners adhere to strict data protection and privacy standards.
+        </p>
+        <h2 className="text-xl font-semibold mb-3">Changes to Our Privacy Policy</h2>
+        <p className="mb-4">
+          Our Privacy Policy may change from time to time. We will post any privacy policy changes on this page and, if the changes are significant, we will provide a more prominent notice.
+        </p>
+        <h2 className="text-xl font-semibold mb-3">Contact Us</h2>
+        <p className="mb-4">
+          If you have any questions or concerns about our privacy policy or practices, please contact us at:
+        </p>
+        <p className="mb-4 font-medium">hojoisaac@gmail.com</p>
+    </div>
+  );
+}
