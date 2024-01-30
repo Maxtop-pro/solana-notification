6a94c73fd9434bd18bfbbf97a4eebef509b098d8
Maxtop <upworkgoes@gmail.com>
2024-01-30 11:37:05 +0100

    Refactor privacy page component

--- src/app/kini/privacy/page.tsx
+++ src/app/kini/privacy/page.tsx
@@ -1,36 +1,39 @@
 import React from "react";
 
-export default function page() {
+export default function Page() {
   return (
     <div className="flex flex-col mt-20 ml-20">
-    <h1 className="text-2xl font-bold mb-4">Kini Privacy Policy</h1>
-        <p className="mb-4">
-          Kini is a dictionary app committed to your privacy. We do not collect any personal data or user identifiers.
-        </p>
-        <h2 className="text-xl font-semibold mb-3">AdMob</h2>
-        <p className="mb-4">
-          We use Google&apos;s AdMob advertising service to display ads within the app. AdMob may collect and use information to provide personalized advertising. This includes non-personally identifiable information specific to the ad serving process.
-        </p>
-        <p className="mb-4">
-            Please refer to Google&apos;s Privacy Policy for more details on how they manage the data they collect.
-        </p>
-        <h2 className="text-xl font-semibold mb-3">Search Queries</h2>
-        <p className="mb-4">
-          Any search queries you make within Kini are processed by OpenAI to provide dictionary results. These queries are not stored by Kini and are only used to generate immediate search results.
-        </p>
-        <h2 className="text-xl font-semibold mb-3">Data Protection</h2>
-        <p className="mb-4">
-          We are dedicated to protecting your data and ensuring that our third-party partners adhere to strict data protection and privacy standards.
-        </p>
-        <h2 className="text-xl font-semibold mb-3">Changes to Our Privacy Policy</h2>
-        <p className="mb-4">
-          Our Privacy Policy may change from time to time. We will post any privacy policy changes on this page and, if the changes are significant, we will provide a more prominent notice.
-        </p>
-        <h2 className="text-xl font-semibold mb-3">Contact Us</h2>
-        <p className="mb-4">
-          If you have any questions or concerns about our privacy policy or practices, please contact us at:
-        </p>
-        <p className="mb-4 font-medium">hojoisaac@gmail.com</p>
+      <h1 className="text-2xl font-bold mb-4">Kini Privacy Policy</h1>
+      <p className="mb-4">
+        Kini is a dictionary app committed to your privacy. We do not collect any personal data or user identifiers.
+      </p>
+      <h2 className="text-xl font-semibold mb-3">AdMob</h2>
+      <p className="mb-4">
+        We use Google&apos;s AdMob advertising service to display personalized ads within the app. AdMob may collect and use information, including non-personally identifiable information, to provide personalized advertising.
+      </p>
+      <p className="mb-4">
+        By using the app, you consent to the collection and processing of your information by AdMob for personalized advertising purposes. However, if you are an EU user, you have the right to opt out of personalized ads by adjusting your device settings or using the ad settings provided by AdMob.
+      </p>
+      <p className="mb-4">
+        Please refer to Google&apos;s Privacy Policy for more details on how they manage the data they collect.
+      </p>
+      <h2 className="text-xl font-semibold mb-3">Search Queries</h2>
+      <p className="mb-4">
+        Any search queries you make within Kini are processed by OpenAI to provide dictionary results. These queries are not stored by Kini and are only used to generate immediate search results.
+      </p>
+      <h2 className="text-xl font-semibold mb-3">Data Protection</h2>
+      <p className="mb-4">
+        We are dedicated to protecting your data and ensuring that our third-party partners adhere to strict data protection and privacy standards.
+      </p>
+      <h2 className="text-xl font-semibold mb-3">Changes to Our Privacy Policy</h2>
+      <p className="mb-4">
+        Our Privacy Policy may change from time to time. We will post any privacy policy changes on this page and, if the changes are significant, we will provide a more prominent notice.
+      </p>
+      <h2 className="text-xl font-semibold mb-3">Contact Us</h2>
+      <p className="mb-4">
+        If you have any questions or concerns about our privacy policy or practices, please contact us at:
+      </p>
+      <p className="mb-4 font-medium">hojoisaac@gmail.com</p>
     </div>
   );
 }
